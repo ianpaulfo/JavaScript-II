2dcc12feaf7bb126a044272732f59227a52d517f
Ian Fragoso <IanPaul@Ians-MacBook-Pro.local>
2019-10-08 17:42:33 -0700

    ready to submit

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,15 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+let me = "ian";
+
+function greeting() {
+
+  console.log("Hello, " + me + "!")
+
+}
+
+me = "everyone";
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
